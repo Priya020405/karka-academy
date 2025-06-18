12084efbd76d7cd6f9715cd310b0e45a7010ac3e
Priya020405 <tharshini623@gmail.com>
2025-06-18 18:03:16 +0530

    Add files via upload

---
+++ Task1/task1.py
@@ -0,0 +1,73 @@
+#1
+#print("Hello,World!")
+
+#2
+# print("Priya Tharshini.S")
+
+# #3
+#print("name:S.Priya Tharshini\n age:20")
+
+# #4
+# name="priya"
+# age=20
+# percentage=8.5
+# student=True
+# print("iam a",name)
+# print("my age is", age  )
+# print("i have passed by",percentage   )
+# print("not a student")
+      
+# # #5
+# name="priya"
+# age=20
+# print(name,age)
+
+# #6
+# name="priya"
+# age=20
+# print("Name:",name)
+# print("Age:",age)
+
+# # #7
+# a=35
+# b=12
+# print("addation",a+b)
+# print("subraction",a-b)
+# print("multiplication",a*b)
+# print("division",a/b)
+
+# #8
+print("*")
+print("**")
+print("***")
+print("****")
+
+# #9
+# name="priya"
+# age=20
+# isstudent=True
+# print("Name=",name)
+# print("Age:",age)
+# print(isstudent)
+# name=("tharshini")
+# age=age+1
+# isstudent=False
+# print("Name=",name)
+# print("Age:",age)
+# print(isstudent)
+
+# #10
+# name="priya"
+# age=20
+# dob="2-4-2005"
+# height=5.4
+# weight=58.2
+# degree="B.E"
+# gender="Female"
+# print("Name:",name)
+# print("Age:",age)
+# print("DOB:",dob)
+# print("Height:",height)
+# print("weight:",weight)
+# print("Degree:",degree)
+# print("Gender:",gender)
