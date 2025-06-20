2cfdc70f1ca2f39f1a07556ebae20574978bcd24
Priya020405 <tharshini623@gmail.com>
2025-06-20 18:05:28 +0530

    Add files via upload

---
+++ Task2 output/task2.py
@@ -0,0 +1,118 @@
+#1
+# studentName="priya"
+# mark_obtained=90
+# print("name",studentName)
+# print("mark",mark_obtained)
+
+#2
+# a=89
+# b=50
+# print("a+b=",a+b)
+
+#3
+# r=15
+# area=3.13*(r**2)
+# print(area)
+
+#4
+# l=int(input("enter the l:"))
+# w=int(input("enter the w:"))
+# area=l*w
+# print(area)
+
+#5
+# b=30
+# h=10
+# area=(b*h)/2
+# print(area)
+
+#6
+# a=380
+# b=97
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+
+#7
+# a=30
+# a+=20
+# print(a)
+# a-=20
+# print(a)
+# a*=20
+# print(a)
+# a/=20
+# print(a)
+# a//=20
+# print(a)
+# a%=20
+# print(a)
+# a**=20
+# print(a)
+
+#8
+# a=80
+# a+=30
+# print(a)
+# b=40
+# b-=10
+# print(b)
+
+#9
+# a=100
+# b=39
+# print(b!=60)
+# print(a==b)
+# print(a<=70)
+# print(60>=a)
+# print(a>54)
+# print(a>b)
+
+#10
+# x=100
+# y=73
+# z=63
+# print(x<=112 and ((y>=20) or not(z!=63)))
+# print((y>=z or not (z==x)) and (x<60 or y>=50))
+# print(z<=200 and y!=65)
+# print(not(y>=63 or x==55) and y!=z)
+# print((z>=50 or x<=60) or not(x==z))
+
+
+#11
+# a=20
+# b=10
+# temp=a
+# a=b
+# b=temp
+# print(a)
+# print(b)
+
+
+#12
+# a=80
+# b=39
+# c=200
+# avg=(a+b+c)/3
+# print(avg)
+
+#13
+# a=290
+# b=203
+# c=108
+# d=170
+# print(((a+b)*c)/d)
+
+
+#14
+tamil=99
+english=100
+maths=100
+science=92
+social=96
+avg=((tamil+english+maths+science+social)/5)
+total=(tamil+english+maths+science+social)
+print(avg)
+print(total)
+
